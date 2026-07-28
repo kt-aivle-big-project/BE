35ef003f82753604cb10c47460707a28b13df149
박병준 <pbjun2000@gmail.com>
2026-07-28 17:45:11 +0900

    feat: support warehouse layout API integration

--- src/main/java/com/aivle/be/global/config/SwaggerConfig.java
+++ src/main/java/com/aivle/be/global/config/SwaggerConfig.java
@@ -1,8 +1,9 @@
 package com.aivle.be.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,17 +13,26 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes(
-                        "bearerAuth",
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                ))
-                .info(new Info()
-                        .title("Warehouse API")
-                        .description("Warehouse backend API documentation")
-                        .version("1.0.0"));
+                .addSecurityItem(
+                        new SecurityRequirement().addList(securitySchemeName)
+                )
+                .components(
+                        new Components().addSecuritySchemes(
+                                securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .info(
+                        new Info()
+                                .title("Warehouse API")
+                                .description("Warehouse backend API documentation")
+                                .version("1.0.0")
+                );
     }
 }
